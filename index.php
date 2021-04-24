369ae27263a3abc69346d4983a0fd1fa568e8c17
mio <omisiya@gmail.com>
2021-04-24 19:53:08 +0900

    alteration-1

--- index.php
+++ index.php
@@ -13,30 +13,6 @@
 <!-- <body> -->
 <!-- ヘッダー -->
 <?php get_header(); ?>
-<!-- <header class="header">
-    <a href="index.html" class="header-logo">
-      <img src="img/estramedia__logo.png" alt="ロゴ">
-    </a>
-    <nav class="header__nav pc">
-      <ul class="header__nav-items flex">
-        <li class="header__nav-list">
-          <a href="category.html" class="header__nav-link">HTML</a>
-        </li>
-        <li class="header__nav-list">
-          <a href="category.html" class="header__nav-link">CSS</a>
-        </li>
-        <li class="header__nav-list">
-          <a href="category.html" class="header__nav-link">JavaScript</a>
-        </li>
-        <li class="header__nav-list">
-          <a href="category.html" class="header__nav-link">PHP</a>
-        </li>
-        <li class="header__nav-list">
-          <a href="category.html" class="header__nav-link">MySQL</a>
-        </li>
-      </ul>
-    </nav>
-  </header> -->
 
 <!-- ファーストビュー -->
 <div class="firstview">
@@ -85,7 +61,6 @@
   <?php else : ?>
     <p>投稿が見つかりません。</p>
   <?php endif; ?>
-
   <!-- <a href="single.html" class="article__card">
               <div class="article__card-img">
                 <img src="img/blog__first.jpg" alt="">
@@ -95,103 +70,17 @@
                 <h2 class="article__card-ttl">プログラミングでフリーランスになるには??</h2>
                 <p class="article__card-date">2021-02-26</p>
               </div>
-            </a> -->
-  <!-- <a href="single.html" class="article__card">
-              <div class="article__card-img">
-                <img src="img/blog__first.jpg" alt="">
-              </div>
-              <span class="article__cat">キャリア</span>
-              <div class="article__card-txt">
-                <h2 class="article__card-ttl">プログラミングでフリーランスになるには??</h2>
-                <p class="article__card-date">2021-02-26</p>
-              </div>
-            </a>
-            <a href="single.html" class="article__card">
-              <div class="article__card-img">
-                <img src="img/blog__first.jpg" alt="">
-              </div>
-              <span class="article__cat">キャリア</span>
-              <div class="article__card-txt">
-                <h2 class="article__card-ttl">プログラミングでフリーランスになるには??</h2>
-                <p class="article__card-date">2021-02-26</p>
-              </div>
-            </a>
-            <a href="single.html" class="article__card">
-              <div class="article__card-img">
-                <img src="img/blog__first.jpg" alt="">
-              </div>
-              <span class="article__cat">キャリア</span>
-              <div class="article__card-txt">
-                <h2 class="article__card-ttl">プログラミングでフリーランスになるには??</h2>
-                <p class="article__card-date">2021-02-26</p>
-              </div>
-            </a>
-            <a href="single.html" class="article__card">
-              <div class="article__card-img">
-                <img src="img/blog__first.jpg" alt="">
-              </div>
-              <span class="article__cat">キャリア</span>
-              <div class="article__card-txt">
-                <h2 class="article__card-ttl">プログラミングでフリーランスになるには??</h2>
-                <p class="article__card-date">2021-02-26</p>
-              </div>
-            </a>
-            <a href="single.html" class="article__card">
-              <div class="article__card-img">
-                <img src="img/blog__first.jpg" alt="">
-              </div>
-              <span class="article__cat">キャリア</span>
-              <div class="article__card-txt">
-                <h2 class="article__card-ttl">プログラミングでフリーランスになるには??</h2>
-                <p class="article__card-date">2021-02-26</p>
-              </div>
-            </a> -->
+        </a> -->
       </div>
     </div>
     <!-- サイドバー  -->
     <?php get_sidebar(); ?>
-    <!-- <aside class="sidebar">
-          <div class="side-banner">
-            <a href=""><img src="img/banner.png" alt=""></a>
-          </div>
-          <div class="side-profile">
-            <div class="profile__img">
-              <img src="img/estramedia__logo.png" alt="">
-            </div>
-            <h2 class="profile__ttl">エストラ編集部</h2>
-            <p class="profile__txt">プロフィール文が入ります。プロフィール文が入ります。プロフィール文が入ります。プロフィール文が入ります。プロフィール文が入ります。
-            </p>
-          </div>
-          <div class="category">
-            <h2 class="category__ttl">Category</h2>
-            <ul>
-              <li class="category__list">
-                <a href="category.html" class="category__link">HTML</a>
-              </li>
-              <li class="category__list">
-                <a href="category.html" class="category__link">CSS</a>
-              </li>
-              <li class="category__list">
-                <a href="category.html" class="category__link">JavaScript</a>
-              </li>
-              <li class="category__list">
-                <a href="category.html" class="category__link">PHP</a>
-              </li>
-              <li class="category__list">
-                <a href="category.html" class="category__link">MySQL</a>
-              </li>
-            </ul>
-          </div>
-        </aside> -->
   </div>
   </div>
 </main>
 
 <!-- フッター -->
 <?php get_footer(); ?>
-<!-- <footer class="footer">
-    <p>Copyright &copy; 2021 coachtechサンプルテスト All Rights Reserved.</p>
-  </footer> -->
-<!-- </body>
 
+<!-- </body>
 </html> -->
